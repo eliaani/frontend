1ddaf5dea389ce25ae5bf3eae687faa5d9cce621
Aatu Marttila <aatumarttila@gmail.com>
2022-12-08 12:18:30 +0200

    Homepage

---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+export default function Home(){
+    return(
+    <div>
+        <h1>This is the main page for dogs clothing store.</h1>
+        <img src="https://images.unsplash.com/photo-1508814437933-f0c7d18a9217?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"/>
+    </div>
+
+    )
+}
